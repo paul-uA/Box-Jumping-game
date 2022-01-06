298df4828171b48d5ccc938c1bffa5d72208e3af
paulua <paulavila54mac@gmail.com>
2022-01-05 19:04:55 -0600

    Well everything works for the MVP. I feel like its cheating to have most of my game in one function.

--- main.js
+++ main.js
@@ -27,26 +27,27 @@ function startTime(){
     }, 500);
 };
 
-function triangleMover (){
-    let moveyMoverson = setInterval(function (){
-        if (triRed.classList != "slide2right"){
-        triRed.classList.add("slide2right");}
-        
+// function triangleMover (){
+//     let moveyMoverson = setInterval(function (){
+//         if (triRed.classList != "slide2right"){
+//         triRed.classList.add("slide2right");}
+                
 
-        let timeout =  setTimeout(function (){
-            //console.log("works");
-            triRed.classList.remove("slide2right");
-        },1300);
+//         let timeout =  setTimeout(function (){
+//             //console.log("works");
+//             triRed.classList.remove("slide2right");
+//         },1300);
                        
-    },1301)   
-
-};
+//     },1301)    
+// };
 
 
 
 function jumpyJumperson (){
     if (boxP.classList !== "boxJumpA"){
-        boxP.classList.add("boxJumpA");}
+        boxP.classList.add("boxJumpA");
+        console.log("jump"); 
+    }
 
         let jtime = setTimeout(function(){
             boxP.classList.remove("boxJumpA");
@@ -54,23 +55,58 @@ function jumpyJumperson (){
 
 };
 
-function death2Tri(){  
+// function death2Tri(){  
+//     let gameOver = setInterval(function(){
+//         if ( (parseInt(window.getComputedStyle(triRed).getPropertyValue('left'))< 81 ) && (parseInt(window.getComputedStyle(triRed).getPropertyValue('left')) > 25)&& (parseInt(window.getComputedStyle(boxP).getPropertyValue('top')) >= 98)){
+//             clearInterval(moveyMoverson);
+//             alert("Game Over!");
+        
+//     }
+//     },10); 
+
+// };
+
+function gameStart(){
+    let youlose = false;
+    
+    // checks if  you lose the game
     let gameOver = setInterval(function(){
         if ( (parseInt(window.getComputedStyle(triRed).getPropertyValue('left'))< 81 ) && (parseInt(window.getComputedStyle(triRed).getPropertyValue('left')) > 25)&& (parseInt(window.getComputedStyle(boxP).getPropertyValue('top')) >= 98)){
             clearInterval(moveyMoverson);
+            youlose = true;
             alert("Game Over!");
         
     }
     },10); 
+   
+    // moves the Triangle
+    let moveyMoverson = setInterval(function (){
+        if (triRed.classList != "slide2right"){
+        triRed.classList.add("slide2right");}
+                
 
+        let timeout =  setTimeout(function (){
+            //console.log("works");
+            triRed.classList.remove("slide2right");
+        },1300);
+                       
+    },1301) 
+    
+    //starts the timeer
+    let tt = 0
+    //console.log("works");
+    if (youlose !== true ){
+        let uptime = setInterval(function (){
+            tt += 1;
+            timeScorer.innerText= tt;
+        }, 500);
+    
+
+    }
+   
+  
 };
 
-function gameStart(){
-    checkMcCheckerson();
-    startTime();
-    death2Tri();
-    triangleMover();
-};
     
  startButton.addEventListener("click", gameStart);
  document.addEventListener("keydown",jumpyJumperson);
